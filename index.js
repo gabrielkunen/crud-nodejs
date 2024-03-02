4901f84b087eab89926340a49211a84b1f5b3796
Gabriel Mariano Kunen <gabriel.marianok@gmail.com>
2024-03-02 20:51:22 -0300

    crud

--- index.js
+++ index.js
@@ -52,4 +52,47 @@ app.get('/usuario', async (_, res) => {
   }
 });
 
+app.get('/usuario/:id', async (req, res) => {
+
+  const client = await pool.connect()
+
+  try {
+      const result = await client.query('SELECT * FROM usuario WHERE id = $1', [req.params.id])
+      res.send(result.rows);
+  } catch (err) {
+      res.status(500).send(err.message);
+  } finally {
+    client.release()
+  }
+});
+
+app.delete('/usuario/:id', async (req, res) => {
+
+  const client = await pool.connect()
+
+  try {
+      await client.query('DELETE FROM usuario WHERE id = $1', [req.params.id])
+      res.json({ message: "Usuário deletado com sucesso" });
+  } catch (err) {
+      res.status(500).send(err.message);
+  } finally {
+    client.release()
+  }
+});
+
+app.put('/usuario/:id', async (req, res) => {
+
+  const client = await pool.connect()
+
+  try {
+
+      await client.query('UPDATE usuario SET nome = $1, senha = $2 WHERE id = $3', [req.body.nome, req.body.senha, req.params.id])
+      res.json({ message: "Usuário atualizado com sucesso" });
+  } catch (err) {
+      res.status(500).send(err.message);
+  } finally {
+    client.release()
+  }
+});
+
 app.listen(3000, () => {});
